40d2c0c7d064e7dca4aa0ec6b04c8a3668c7c42c
kiz75 <kiz75@DESKTOP-5KTKKGJ>
2019-11-07 19:35:12 +0900

    doc: java commit

--- DBP_Project/recipeProject/src/recipeProject/TestClass.java
+++ DBP_Project/recipeProject/src/recipeProject/TestClass.java
@@ -4,7 +4,7 @@ public class TestClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("잘 보이니 애들아??");
+		System.out.println("hello??");
 	}
 
 }
